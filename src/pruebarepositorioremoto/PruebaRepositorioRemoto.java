b2d1a51f9af8680e2cccc2b30111084dc7b9a410
Nelson Hernandez <Nelson Hernandez@DESKTOP-22AR8L1>
2019-11-14 22:13:38 -0600

    primer repositorio netbeans-

---
+++ src/pruebarepositorioremoto/PruebaRepositorioRemoto.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebarepositorioremoto;
+
+/**
+ *
+ * @author Nelson Hernandez
+ */
+public class PruebaRepositorioRemoto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("Guardar el codigo en GitHub");
+    }
+    
+}
